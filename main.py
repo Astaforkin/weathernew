8db9f70a3cbc06900b84cbae4a01f40c1ea884b9
Nikita Astaforkin <astaforkin90@gmail.com>
2022-06-17 10:34:30 +0300

    format main.py and config.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import psycopg2
 import requests
 from config import config
 
+
 def create_tables():
     commands = (
         """
@@ -9,11 +10,12 @@ def create_tables():
                 id serial PRIMARY KEY,
                 city varchar(50) NOT NULL,
                 date varchar(50) UNIQUE NOT NULL,
-                weather varchar(50) UNIQUE NOT NULL,
-                temp_max varchar(50) UNIQUE NOT NULL,
-                temp_min varchar(50) UNIQUE NOT NULL
+                weather varchar(50) NOT NULL,
+                temp_max varchar(50) NOT NULL,
+                temp_min varchar(50) NOT NULL
         )
-        """,)
+        """,
+    )
     conn = None
     try:
         params = config()
@@ -30,7 +32,6 @@ def create_tables():
             conn.close()
 
 
-
 def insert_weather(date, weather, temp_max, temp_min):
     sql = "INSERT INTO weather (city, date, weather, temp_max, temp_min) VALUES('Ryazan',%s ,%s, %s, %s)"
     conn = None
@@ -38,7 +39,15 @@ def insert_weather(date, weather, temp_max, temp_min):
         params = config()
         conn = psycopg2.connect(**params)
         cur = conn.cursor()
-        cur.execute(sql, (date, weather, temp_max, temp_min,))
+        cur.execute(
+            sql,
+            (
+                date,
+                weather,
+                temp_max,
+                temp_min,
+            ),
+        )
         conn.commit()
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -47,18 +56,20 @@ def insert_weather(date, weather, temp_max, temp_min):
         if conn is not None:
             conn.close()
 
+
 def insert_table():
-    url = f'https://www.7timer.info/bin/civillight.php?lon=39&lat=54&ac=0&unit=metric&output=json&tzshift=0'
+    url = f"https://www.7timer.info/bin/civillight.php?lon=39&lat=54&ac=0&unit=metric&output=json&tzshift=0"
     response = requests.get(url)
     if response.status_code == 200:
         response_data = response.json()
-        for record in response_data['dataseries']:
-            date = record['date']
-            weather = record['weather']
-            temp_max = record['temp2m']['max']
-            temp_min = record['temp2m']['min']
+        for record in response_data["dataseries"]:
+            date = record["date"]
+            weather = record["weather"]
+            temp_max = record["temp2m"]["max"]
+            temp_min = record["temp2m"]["min"]
             insert_weather(date, weather, temp_max, temp_min)
-                       
+
+
 def update_table():
     sql = "ALTER TABLE weather DROP CONSTRAINT weather_temp_min_key;"
     conn = None
@@ -66,7 +77,9 @@ def update_table():
         params = config()
         conn = psycopg2.connect(**params)
         cur = conn.cursor()
-        cur.execute(sql,)
+        cur.execute(
+            sql,
+        )
         conn.commit()
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -74,7 +87,7 @@ def update_table():
     finally:
         if conn is not None:
             conn.close()
-    
 
-if __name__ == '__main__':
-    update_table()
+
+if __name__ == "__main__":
+    insert_table()
