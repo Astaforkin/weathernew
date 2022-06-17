95065edddeb18935d9db00eb30f621cb073e54a3
Nikita Astaforkin <astaforkin90@gmail.com>
2022-06-17 09:24:39 +0300

    add update_table() and insert_table() in main.py and fix main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from config import config
 def create_tables():
     commands = (
         """
-        CREATE TABLE IF NOT EXISTS weather(
+        CREATE TABLE IF NOT EXISTS weather (
                 id serial PRIMARY KEY,
                 city varchar(50) NOT NULL,
                 date varchar(50) UNIQUE NOT NULL,
@@ -13,7 +13,7 @@ def create_tables():
                 temp_max varchar(50) UNIQUE NOT NULL,
                 temp_min varchar(50) UNIQUE NOT NULL
         )
-        """)
+        """,)
     conn = None
     try:
         params = config()
@@ -32,7 +32,7 @@ def create_tables():
 
 
 def insert_weather(date, weather, temp_max, temp_min):
-    sql = "INSERT INTO weather(city, date, weather, temp_max, temp_min) VALUES('Ryazan',%s ,%s, %s, %s)"
+    sql = "INSERT INTO weather (city, date, weather, temp_max, temp_min) VALUES('Ryazan',%s ,%s, %s, %s)"
     conn = None
     try:
         params = config()
@@ -46,9 +46,8 @@ def insert_weather(date, weather, temp_max, temp_min):
     finally:
         if conn is not None:
             conn.close()
-                       
-if __name__ == '__main__':
-    create_tables()
+
+def insert_table():
     url = f'https://www.7timer.info/bin/civillight.php?lon=39&lat=54&ac=0&unit=metric&output=json&tzshift=0'
     response = requests.get(url)
     if response.status_code == 200:
@@ -59,4 +58,23 @@ if __name__ == '__main__':
             temp_max = record['temp2m']['max']
             temp_min = record['temp2m']['min']
             insert_weather(date, weather, temp_max, temp_min)
+                       
+def update_table():
+    sql = "ALTER TABLE weather DROP CONSTRAINT weather_temp_min_key;"
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute(sql,)
+        conn.commit()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
     
+
+if __name__ == '__main__':
+    update_table()
