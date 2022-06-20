e6ac8e3f9fc8c8db78c68afe989a5216b1546fcb
Nikita Astaforkin <astaforkin90@gmail.com>
2022-06-20 12:16:55 +0300

    add def run_sql()

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from mysqlx import Statement
 import psycopg2
 import requests
 from config import config
@@ -8,11 +9,12 @@ def create_tables():
         """
         CREATE TABLE IF NOT EXISTS weather (
                 id serial PRIMARY KEY,
-                city varchar(50) UNIQUE NOT NULL,
-                date varchar(50) UNIQUE NOT NULL,
+                city varchar(50) NOT NULL,
+                date varchar(50) NOT NULL,
                 weather varchar(50) NOT NULL,
                 temp_max varchar(50) NOT NULL,
-                temp_min varchar(50) NOT NULL
+                temp_min varchar(50) NOT NULL,
+                UNIQUE (city, date)
         )
         """,
     )
@@ -69,16 +71,15 @@ def insert_table():
             temp_min = record["temp2m"]["min"]
             insert_weather(date, weather, temp_max, temp_min)
 
-
-def update_table():
-    sql = "ALTER TABLE weather DROP CONSTRAINT weather_temp_min_key;"
+def run_sql(statement, arguments):
     conn = None
     try:
         params = config()
         conn = psycopg2.connect(**params)
         cur = conn.cursor()
         cur.execute(
-            sql,
+            statement,
+            arguments
         )
         conn.commit()
         cur.close()
@@ -88,6 +89,17 @@ def update_table():
         if conn is not None:
             conn.close()
 
-
+def ryazan_dataseries():
+    url = f"https://www.7timer.info/bin/civillight.php?lon=39&lat=54&ac=0&unit=metric&output=json&tzshift=0"
+    response = requests.get(url)
+    if response.status_code == 200:
+        response_data = response.json()
+        for record in response_data["dataseries"]:
+            date = record["date"]
+            weather = record["weather"]
+            temp_max = record["temp2m"]["max"]
+            temp_min = record["temp2m"]["min"]
+            weatherdict = dict(city = 'Ryazan', date = record["date"], weather = record["weather"], temp_max = record["temp2m"]["max"], temp_min = record["temp2m"]["min"])
+    return weatherdict
 if __name__ == "__main__":
-    insert_table()
+    run_sql("INSERT INTO weather (city, date, weather, temp_max, temp_min) VALUES(%(city)s ,%(date)s ,%(weather)s, %(temp_max)s, %(temp_min)s)", ryazan_dataseries())
