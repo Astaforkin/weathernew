46e0203ac74022c06d8f7246ec924c113f4f1018
Nikita Astaforkin <astaforkin90@gmail.com>
2022-06-21 12:43:23 +0300

    add list cities, add in get_data_from_weather_api() arguments lon,lat, fix insert_data_into_db() variable city in weatherdict

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from array import array
 from ast import arguments
 from mysqlx import Statement
 import psycopg2
 import requests
+from array import *
 from config import config
 
 
@@ -72,16 +74,14 @@ def insert_table():
             temp_min = record["temp2m"]["min"]
             insert_weather(date, weather, temp_max, temp_min)
 
+
 def run_sql(statement, arguments):
     conn = None
     try:
         params = config()
         conn = psycopg2.connect(**params)
         cur = conn.cursor()
-        cur.execute(
-            statement,
-            arguments
-        )
+        cur.execute(statement, arguments)
         conn.commit()
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
@@ -90,33 +90,32 @@ def run_sql(statement, arguments):
         if conn is not None:
             conn.close()
 
-# def get_data_from_weather_api(lon, lat):
-#     url = f'https://www.7timer.info/bin/civillight.php?lon={lon}&lat={lat}&ac=0&unit=metric&output=json&tzshift=0'
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         response_data = response.json()
-#         for record in response_data["dataseries"]:
-#             date = record["date"]
-#             weather = record["weather"]
-#             temp_max = record["temp2m"]["max"]
-#             temp_min = record["temp2m"]["min"]
-#             weatherdict = dict(city = 'Ryazan', date = record["date"], weather = record["weather"], temp_max = record["temp2m"]["max"], temp_min = record["temp2m"]["min"])
-#             return weatherdict
 
-
-def get_data_from_weather_api():
-    url = f'https://www.7timer.info/bin/civillight.php?lon=39&lat=54&ac=0&unit=metric&output=json&tzshift=0'
+def get_data_from_weather_api(lon, lat):
+    url = f"https://www.7timer.info/bin/civillight.php?lon={lon}&lat={lat}&ac=0&unit=metric&output=json&tzshift=0"
     response = requests.get(url)
     if response.status_code == 200:
         response_data = response.json()
-        return response_data['dataseries']
+        return response_data["dataseries"]
+
 
 def insert_data_into_db():
-    for record in get_data_from_weather_api():
-            statement = "INSERT INTO weather (city, date, weather, temp_max, temp_min) VALUES(%(city)s ,%(date)s ,%(weather)s, %(temp_max)s, %(temp_min)s)"
-            weatherdict = dict(city = 'Ryazan', date = record["date"], weather = record["weather"], temp_max = record["temp2m"]["max"], temp_min = record["temp2m"]["min"])
-            run_sql(statement, weatherdict)    
+    for record in get_data_from_weather_api(lon, lat):
+        statement = "INSERT INTO weather (city, date, weather, temp_max, temp_min) VALUES(%(city)s ,%(date)s ,%(weather)s, %(temp_max)s, %(temp_min)s)"
+        weatherdict = dict(
+            city = cityname,
+            date=record["date"],
+            weather=record["weather"],
+            temp_max=record["temp2m"]["max"],
+            temp_min=record["temp2m"]["min"],
+        )
+        run_sql(statement, weatherdict)
 
+
+cities = [{"name": "Ryazan", "lon": "39", "lat": "54"}, {"name": "Moscow", "lon": "37.36", "lat": "54.44"}]
 
 if __name__ == "__main__":
+    lon = cities[1]['lon']
+    lat = cities[1]['lat']
+    cityname = cities[1]['name']
     insert_data_into_db()
