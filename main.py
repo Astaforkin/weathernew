1e5b57d5f761ade2003d84822fefb61dec0bd1cc
Nikita Astaforkin <astaforkin90@gmail.com>
2022-06-23 12:38:44 +0300

    fix def insert_data_into_db()

--- main.py
+++ main.py
@@ -34,44 +34,6 @@ def create_tables():
             conn.close()
 
 
-def insert_weather(date, weather, temp_max, temp_min):
-    sql = "INSERT INTO weather (city, date, weather, temp_max, temp_min) VALUES('Ryazan',%s ,%s, %s, %s)"
-    conn = None
-    try:
-        params = config()
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()
-        cur.execute(
-            sql,
-            (
-                date,
-                weather,
-                temp_max,
-                temp_min,
-            ),
-        )
-        conn.commit()
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-
-
-def insert_table():
-    url = f"https://www.7timer.info/bin/civillight.php?lon=39&lat=54&ac=0&unit=metric&output=json&tzshift=0"
-    response = requests.get(url)
-    if response.status_code == 200:
-        response_data = response.json()
-        for record in response_data["dataseries"]:
-            date = record["date"]
-            weather = record["weather"]
-            temp_max = record["temp2m"]["max"]
-            temp_min = record["temp2m"]["min"]
-            insert_weather(date, weather, temp_max, temp_min)
-
-
 def run_sql(statement, arguments):
     conn = None
     try:
@@ -96,33 +58,18 @@ def get_data_from_weather_api(lon, lat):
         return response_data["dataseries"]
 
 
-def insert_data_into_db(index):
-    lon = cities[index]["lon"]
-    lat = cities[index]["lat"]
-    cityname = cities[index]["name"]
-    for record in get_data_from_weather_api(lon, lat):
-        statement = "INSERT INTO weather (city, date, weather, temp_max, temp_min) VALUES(%(city)s ,%(date)s ,%(weather)s, %(temp_max)s, %(temp_min)s)"
-        weatherdict = dict(
-            city=cityname,
-            date=record["date"],
-            weather=record["weather"],
-            temp_max=record["temp2m"]["max"],
-            temp_min=record["temp2m"]["min"],
-        )
-        run_sql(statement, weatherdict)
-
-
-def insert_data_into_update_set_db(cityname, lon, lat):
-    for record in get_data_from_weather_api(lon, lat):
+def insert_data_into_db(weather_data):
+    for record in weather_data:
         statement = "INSERT INTO weather (city, date, weather, temp_max, temp_min) VALUES(%(city)s ,%(date)s ,%(weather)s, %(temp_max)s, %(temp_min)s) ON CONFLICT (city, date) DO UPDATE SET weather=EXCLUDED.weather, temp_max=EXCLUDED.temp_max, temp_min=EXCLUDED.temp_min"
         weatherdict = dict(
-            city=cityname,
+            city=city['name'],
             date=record["date"],
             weather=record["weather"],
             temp_max=record["temp2m"]["max"],
             temp_min=record["temp2m"]["min"],
         )
         run_sql(statement, weatherdict)
+
 
 
 cities = [
@@ -132,5 +79,7 @@ cities = [
 ]
 
 if __name__ == "__main__":
+    create_tables()
     for city in cities:
-        insert_data_into_update_set_db(city['name'], city['lon'], city['lat'])
+        weather_data = get_data_from_weather_api(city['lon'], city['lat'])
+        insert_data_into_db(weather_data)
